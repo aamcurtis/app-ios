b6e23902bd52a022060b3549c23bf751e43a9dde
Hamish Rodda <hamish.rodda@easternhealth.org.au>
2020-03-28 08:52:51 +1300

    Closer to matching the GO backend - switch to cenkeys address

--- CoEpi/network/NetworkManager.swift
+++ CoEpi/network/NetworkManager.swift
@@ -11,21 +11,27 @@ import Alamofire
 
 struct ExposureCheck: Encodable {
     let lastChecked: Date
-    let geoHash: String
+    //let geoHash: String
 }
 
 class NetworkManager {
   var lastSuccessfulExposureCheck = Date()
+  var currentAttemptedExposureCheck = Date()
   
   func getRecentExposures() -> Void {
   
-    let newCheck = ExposureCheck(lastChecked: lastSuccessfulExposureCheck, geoHash: "")
+    let newCheck = ExposureCheck(lastChecked: lastSuccessfulExposureCheck)//, geoHash: "")
     
-    AF.request("https://coepi.wolk.com:8080/exposureandsymptoms",
+    // Set the current attempted exposure check date/time
+    currentAttemptedExposureCheck = Date.init()
+    
+    AF.request("https://coepi.wolk.com:8080/cenkeys",
                method: .post,
                parameters: newCheck,
                encoder: JSONParameterEncoder.default).response { response in
       debugPrint(response)
+                
+      self.lastSuccessfulExposureCheck = self.currentAttemptedExposureCheck
     }
   }
 
