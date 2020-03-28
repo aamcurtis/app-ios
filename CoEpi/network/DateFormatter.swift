505873a377e68772ac374dd7e321329202da9f61
Hamish Rodda <hamish.rodda@easternhealth.org.au>
2020-03-28 17:17:24 +1300

    Attempt to make a DateFormatter to take Unix inputs.  No idea if it will work

--- CoEpi/network/DateFormatter.swift
+++ CoEpi/network/DateFormatter.swift
@@ -7,10 +7,22 @@
 
 import Foundation
 
-extension DateFormatter {
+/*extension DateFormatter {
     static var unixDateFormatter: DateFormatter {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
         return formatter
     }
+}*/
+
+class UnixDateFormatter : DateFormatter {
+
+  override func getObjectValue(_ obj: AutoreleasingUnsafeMutablePointer<AnyObject?>?, for string: String, range rangep: UnsafeMutablePointer<NSRange>?) throws {
+    if let secSince1970 = Double(string) {
+      let convertedDate: AnyObject = Date(timeIntervalSince1970: secSince1970) as NSDate
+      
+      obj?.pointee = convertedDate
+    }
+  }
+
 }
