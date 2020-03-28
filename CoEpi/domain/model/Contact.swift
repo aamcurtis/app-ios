df355ce8e0bfe250776202bc6b2dcfaa7b5de7ea
Hamish Rodda <hamish.rodda@easternhealth.org.au>
2020-03-28 17:55:34 +1300

    Create SHA256 hash (CEN key).  Not sure if inputs are right

--- CoEpi/domain/model/Contact.swift
+++ CoEpi/domain/model/Contact.swift
@@ -1,6 +1,18 @@
 import Foundation
+import CryptoKit
 
 struct Contact : Codable {
     let cen: String
     let date: Date
+  
+  func createCenKey() -> String {
+    
+    let dateString = date.timeIntervalSince1970.description
+    
+    let mash = "\(cen)\(dateString)"
+    let mashData = mash.data(using: .utf8)!
+    
+    let hash = SHA256.hash(data: mashData).description
+    return hash
+  }
 }
